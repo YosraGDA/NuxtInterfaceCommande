755690340cd33bf2d1eefdb846e375fb91f9ba27
YosraGD <yosraguedda@gmail.com>
2024-01-15 22:10:30 +0100

    Ajouter fichier Produit.vue

---
+++ components/Produit.vue
@@ -0,0 +1,18 @@
+<template>
+         
+       <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+         <div class="card-header">{{ nom }}</div>
+     <div class="card-body">
+       <p class="card-text">Prix : {{ prix }} </p>
+       <hr>
+       <button v-if="role== 'Commander'"  @click="$emit('commande', nom, prix)" class="btn btn-success">Commander!</button>
+     </div>
+     <nuxt />
+        </div>
+    
+   </template>
+
+   <script setup >
+     const props = defineProps(["nom","prix","role"]);
+          
+  </script>
--- pages/commande.vue
+++ pages/commande.vue
@@ -1,21 +1,26 @@
 <template>
  <div>  
+  
+    <h1 class="display-1"></h1>
+    <h2>Produits disponible</h2>
     <div class="card-deck">
-     
-    <h2 class= "display-4">Les produits disponibles</h2>
-    <div v-for="produit in produits" class="card text-bg-dark mb-3" style="max-width: 18rem;">
-      <div class="card-header">{{ produit.nom }}</div>
-  <div class="card-body">
-    <p class="card-text">Prix : {{ produit.prix }} </p>
+   <Produit v-for="produit in produits" v-bind:nom="produit.nom"  v-bind:prix="produit.prix"  role="Commander" v-on:commande="ajouterProduit"/>
   </div>
-     </div>
-  <nuxt />
-  </div>
+
+    <h2>Produits commandés</h2>
+    <div class="card-deck">
+     <Produit v-for="produit in commandes" v-bind:nom="produit.nom" v-bind:prix="produit.prix" role="Affichage" />
+    </div>
+    <nuxt />
 </div>
 </template>
 
 <script scoped>
+import Produit from '@/components/Produit.vue'
 export default{
+   components:{
+    Produit
+   },
     data(){
         return{
             produits:[
@@ -23,9 +28,18 @@ export default{
                 {nom:"Frites", prix:2.0},
                 {nom:"Humburger", prix:2.5},
                 {nom:"cheesburger", prix:3.0}
+            ],
+            commandes:[
+                {nom:"Pizza", prix:3.0}
             ]
         }
+    },
+    methods:{
+        ajouterProduit(nom,prix){
+            this.commandes.push({nom:nom, prix:prix})
+        }
     }
+
 }
 </script>
 
